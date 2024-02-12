3e71edf05ece0ee6f4e43ca7299d4c84635bfa62
bohrjomi <aqaminerala@mail.ru>
2024-02-12 10:41:41 +0200

    route-dev

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,49 +1,75 @@
 import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import imglogo from './assets/reacticon.png';
+import './Login.css'
 
 const Login = () => {
-
     const [myLogin, setMyLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [authenticated, setAuthenticated] = useState(false);
 
     function handleLoginChange(event) {
-      setMyLogin(event.target.value);
+        setMyLogin(event.target.value);
     }
-  
-    function handleExport() {
-      console.log(myLogin);
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
     }
-  return (
-    <>
-    <header className="header">
-      <img src={img1} alt="123" className="header-img" />
-      <div className="header-div">
-        <p className="header-p">ReactApp</p>
-      </div>
-    </header>
-    <div className="main-div">
-      <div>
-        <form>
-          <p className="field">
-            <input
-              type="text"
-              name="login"
-              placeholder="Логин"
-              onChange={handleLoginChange}
-            />
-          </p>
-          <p className="field">
-            <input type="password" name="password" placeholder="Пароль" />
-          </p>
-          <p className="submit">
-            <button type="button" name="submit" onClick={handleExport}>
-              Войти
-            </button>
-          </p>
-        </form>
-        <Link to="/login">Войти</Link>
-      </div>
-    </div>
-  </>
-  );
+
+    function handleExport(event) {
+        event.preventDefault();
+
+
+        if (myLogin === 'Sigma' && password === 'Criper') {
+            setAuthenticated(true);
+        } else {
+            console.log('Неверный логин или пароль');
+        }
+    }
+
+    if (authenticated) {
+        return <Navigate to="/Home" />;
+    }
+
+    return (
+        <>
+            <header className="header">
+                <img src={imglogo} alt="123" className="header-img" />
+                <div className="header-div">
+                    <p className="header-p">ReactApp</p>
+                </div>
+            </header>
+            <div className="main-div">
+                <div>
+                    <form onSubmit={handleExport}>
+                        <p className="field">
+                            <input
+                                type="text"
+                                name="login"
+                                placeholder="Логин"
+                                value={myLogin}
+                                onChange={handleLoginChange}
+                            />
+                        </p>
+                        <p className="field">
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Пароль"
+                                value={password}
+                                onChange={handlePasswordChange}
+                            />
+                        </p>
+                        <p className="submit">
+                            <button type="submit" name="submit">
+                                Войти
+                            </button>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Login;
